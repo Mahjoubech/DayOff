ea50cb6cb96f6fbe6284936fbbd4772c79071a6e
Mahjoubech <charkaouielmahjoub50@gmail.com>
2026-04-24 15:57:02 +0100

    DAYOF-53 : add initial unit test for Dayoff application context loading

---
+++ backend/src/test/java/io/github/youco/dayoff/DayoffApplicationTests.java
@@ -0,0 +1,15 @@
+package io.github.youco.dayoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DayoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
